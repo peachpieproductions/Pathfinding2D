0840ee1b0ad7ed1bed8816900e74ee01623c6441
peachpieproductions <benjames.im@gmail.com>
2019-03-20 13:21:45 -0400

    Minor Changes

--- A-Star Pathfinding/Assets/Example/Scripts/Editor/MainManagerEditor.cs
+++ A-Star Pathfinding/Assets/Example/Scripts/Editor/MainManagerEditor.cs
@@ -15,10 +15,12 @@ public class MainManagerEditor : Editor {
         GUI.color = new Color(.8f, 1, .8f);
         if (GUILayout.Button("Set Play Mode")) {
             manager.playerMoney = 50;
+            manager.playerHp = 10;
         }
         GUI.color = new Color(1f, .8f, .8f);
         if (GUILayout.Button("Set Debug Mode")) {
             manager.playerMoney = 500;
+            manager.playerHp = 100;
         }
         GUI.color = Color.white;
         GUILayout.EndHorizontal();
